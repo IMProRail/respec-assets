e6549587bd7287a95d354150e34907c10593091c
IMProRail <arnoud.deboer@prorail.nl>
2026-09-22 15:20:37 +0200

    Create pdf.js

---
+++ js/pdf.js
@@ -0,0 +1,36 @@
+const puppeteer = require('puppeteer');
+const FILE_NAME = process.argv[2];
+
+(async () => {
+    // The following is based on code from
+    // https://www.bannerbear.com/blog/how-to-convert-html-into-pdf-with-node-js-and-puppeteer/
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const website_url = `http://localhost:8080/${FILE_NAME}`;
+    await page.goto(website_url, { waitUntil: 'networkidle0' });
+
+    const pdfName = await page.evaluate(() => {
+      const element = document.getElementById('initialUserConfig');
+      const initialUserConfig = JSON.parse(element.innerText);
+      let pdfSuffix = ".pdf";
+      if (initialUserConfig.fileName !== "") {
+        pdfSuffix = `-${initialUserConfig.fileName.split("\.")[0]}.pdf`
+      }
+      return `${initialUserConfig.pubDomain}-${initialUserConfig.shortName.replace(/\//, '-')}-${initialUserConfig.publishVersion}${pdfSuffix}`;
+    });
+    if (typeof pdfName !== 'string') {
+      throw new Error(`Couldn't obtain the name in ${FILE_NAME}. Got ${pdfName} instead`);
+    }
+    console.log(`Printing PDF for ${FILE_NAME} with path ${pdfName}`);
+
+    await page.emulateMediaType('print');
+    await page.addStyleTag({ content: '.sidelabel {position: absolute}' })
+    await page.pdf({
+      path: pdfName,
+      margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
+      printBackground: true,
+      format: 'A4',
+    });
+
+    await browser.close();
+})();
