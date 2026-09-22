62c43ab03a2e3fca3d81bb909e499b4ca307f901
IMProRail <arnoud.deboer@prorail.nl>
2026-09-22 13:13:47 +0200

    Update prorail-config.mjs

--- js/prorail-config.mjs
+++ js/prorail-config.mjs
@@ -385,8 +385,9 @@ export function loadRespecWithConfiguration(localConfig) {
   ];
 
   globalThis.respecConfig = respecConfig;
+ 
 
- // import(
- //   "https://logius-standaarden.github.io/publicatie/respec/builds/respec-nlgov.js"
-//  );
+  import(
+    "https://gitdocumentatie.logius.nl/publicatie/respec/builds/respec-nlgov.js"
+  );
 }
